0bcf61612a3b810213c3a57c8328473011630864
JBLN <jashbalon@gmail.com>
2025-11-12 14:08:26 +0100

    add options img loading

--- src/utils/getImageURL.js
+++ src/utils/getImageURL.js
@@ -1,14 +1,17 @@
 function getImageURL(name) {
+    const loadProd_force = true
+    const default_img = 'placeHolder.png'
+
     let imgPath
 
-    if(import.meta.env.DEV) {
-        imgPath = new URL(`../assets/images/${name ? name : 'placeHolder.png'}`, import.meta.url).href
+    if(!loadProd_force && import.meta.env.DEV) {
+        imgPath = new URL(`../assets/images/${name || default_img}`, import.meta.url).href
     }
     else {
-        imgPath = `http://localhost:3000/api/uploads/${name}`
+        imgPath = `http://localhost:3000/api/uploads/${name || default_img}`
     }
 
-    console.log("image requested to :", imgPath)
+    console.log("image requested to :", imgPath)    // TODO : remove when tested on prod
     return imgPath
 }
 
