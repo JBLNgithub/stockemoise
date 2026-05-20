bd248148a9d4861d5a9e4a1dafd8859b7395207e
JBLN <jashbalon@gmail.com>
2026-05-20 12:39:00 +0200

    add loading text

--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -2,45 +2,48 @@ import {useState, useEffect, useContext} from 'react'
 import ArticleTile from '../components/ArticleTile'
 import {getPlanning} from '../controllers/planning'
 import { OnAllConcerts } from '../contexts/controlPanelContexts'
+import ArticleTiles from '../components/article-tiles/ArticleTiles'
 
 
 const PlanningPage = () => {
-  const [planning, setPlanning] = useState([])
-  const [onAllConcerts, setOnAllConcerts] = useContext(OnAllConcerts)
-  const [isLoading, setIsLoading] = useState(true)
+    const [planning, setPlanning] = useState([])
+    const [onAllConcerts, setOnAllConcerts] = useContext(OnAllConcerts)
+    const [isLoading, setIsLoading] = useState(true)
 
-  const unmountCleanup = () => {
-    setOnAllConcerts(false)
-  }
-
-  useEffect(() => {
-    const fetchPlanning = async() => {
-      setPlanning(await getPlanning())
-      setIsLoading(false)
+    const unmountCleanup = () => {
+        setOnAllConcerts(false)
     }
-    
-    setOnAllConcerts(true)
-    fetchPlanning()
-    return unmountCleanup
-  }, [])
 
-  return (
-    <>
-      <h1 className='text-5xl font-bold mb-10'>Planning</h1>
+    useEffect(() => {
+        const fetchPlanning = async() => {
+        setPlanning(await getPlanning())
+        setIsLoading(false)
+        }
+        
+        setOnAllConcerts(true)
+        fetchPlanning()
+        return unmountCleanup
+    }, [])
 
-      <section className='grid grid-cols-2 gap-5'>
+    return (
+        <>
+            <h1 className='text-5xl font-bold mb-10'>Planning</h1>
 
-        {!isLoading && planning.map((p) => <ArticleTile 
-          type={p.type}
-          key={`${p.id}${p.type}`} 
-          id={p.id} 
-          title={p.title} 
-          cover={p.cover}
-          date={p.dateEvent}
-        />)}
-      </section>
-    </>
-  )
+                {isLoading ? 
+                    <p>loading...</p>
+                    : <section className='grid grid-cols-2 gap-5'>{
+                        planning.map((p) => <ArticleTile 
+                            type={p.type}
+                            key={`${p.id}${p.type}`} 
+                            id={p.id} 
+                            title={p.title} 
+                            cover={p.cover}
+                            date={p.dateEvent}
+                        />)}
+                    </section>
+                }
+        </>
+    )
 }
 
 export default PlanningPage
